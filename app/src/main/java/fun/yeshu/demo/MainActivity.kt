96938295ef5281ef4a57e3026d6edb1944bac4cf
huhaipeng <huhaipeng@huami.com>
2020-11-20 17:11:20 +0800

    添加同步数据页面

--- app/src/main/java/fun/yeshu/demo/MainActivity.kt
+++ app/src/main/java/fun/yeshu/demo/MainActivity.kt
@@ -1,17 +1,11 @@
 package `fun`.yeshu.demo
 
-import `fun`.yeshu.domain.usecase.SyncModelDataFromDeviceUseCase
 import `fun`.yeshu.ui.ModelDetailActivity
-import `fun`.yeshu.ui.ModelListActivity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.tv_status
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import org.koin.android.ext.android.getKoin
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,11 +14,12 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun onClickSyncDeviceData(view: View) {
-        GlobalScope.launch {
+       /* GlobalScope.launch {
             val useCase = getKoin().get<SyncModelDataFromDeviceUseCase>()
             useCase.execute(System.currentTimeMillis())
             showStatus("sync device data success!")
-        }
+        }*/
+        startActivity(Intent(this, SyncDataActivity::class.java))
     }
 
     private fun showStatus(msg: String) {
@@ -36,7 +31,7 @@ class MainActivity : AppCompatActivity() {
     }
     fun onClickSyncDataToServer(view: View) {}
     fun goToModelListActivity(view: View) {
-        startActivity(Intent(this, ModelListActivity::class.java))
+        startActivity(Intent(this, SyncDataActivity::class.java))
     }
 
     fun goToModelDetailActivity(view: View) {
